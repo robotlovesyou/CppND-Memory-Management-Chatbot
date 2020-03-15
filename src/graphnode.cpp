a6bd5e9f56a331995b0c5d85c0bd5d488ad8d8c7
Andrew Smith <andy@robotlovesyou.com>
2020-03-15 15:08:52 +0100

    add .gitignore. don't dounle delete the chatbot

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // TODO: Remove this dead code if this is the correct approach
+    // delete _chatBot;
 
     ////
     //// EOF STUDENT CODE
